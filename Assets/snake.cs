7738312514e4358412d559e6cd493501ea0c4c95
lacosta26 <lacosta26@amherst.edu>
2024-09-09 13:28:39 +0200

    broken movement

--- Assets/snake.cs
+++ Assets/snake.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class snake : MonoBehaviour
 {
 
+    private Vector3 lastMousePos = new Vector3(1000, 1000, 1000);
     public float speed = 5;
     private Rigidbody2D myRigid;
     // Start is called before the first frame update
@@ -16,10 +17,24 @@ public class snake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-        mousePos = mousePos - transform.position;
-        mousePos = mousePos.normalized;
-        myRigid.velocity = mousePos * speed;
+        
+        if (Input.GetMouseButton(0))
+        {
+            lastMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lastMousePos.z = 0;
+            lastMousePos = lastMousePos - transform.position;
+            lastMousePos = lastMousePos.normalized;
+            myRigid.velocity = lastMousePos * speed;
+
+        } else if (Vector3.Distance(lastMousePos, transform.position) < speed / 10)
+        {
+            transform.position = lastMousePos;
+            myRigid.velocity = Vector3.zero;
+        } 
+        else
+        {
+            Debug.Log(transform.position);
+            Debug.Log(Vector3.Distance(lastMousePos, transform.position));
+        }
     }
 }
